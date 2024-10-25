8ea9130e8349fc9e932b4eb51501ca72b334be5e
Reza Hanif Kurniawan <111202214644@mhs.dinus.ac.id>
2024-10-25 09:58:05 +0800

    Menambahkan Visualisasi Test set result

--- implementation_mycase/naive_bayes.py
+++ implementation_mycase/naive_bayes.py
@@ -49,3 +49,25 @@ plt.xlabel('Komponen Utama 1')
 plt.ylabel('Komponen Utama 2')
 plt.legend()
 plt.show()
+
+
+# Visualising the Test set results
+X_set, y_set = X_test_pca, pp.y_test
+# Membuat meshgrid untuk visualisasi area decision boundary
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# Membatasi sumbu x dan y sesuai dengan range data
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+# Plot titik-titik data pada plot
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+# Menambahkan judul dan label
+plt.title('Naive Bayes (Training set - PCA)')
+plt.xlabel('Komponen Utama 1')
+plt.ylabel('Komponen Utama 2')
+plt.legend()
+plt.show()
